9662e06b9e4b7741fa2a06e6d6188723a3ff4f8b
Abby Demarest <abbyjoydemarest@gmail.com>
2019-04-15 15:18:44 -0400

    added django_heroku to settings.py. provided path for staticfiles

--- support_me/settings.py
+++ support_me/settings.py
@@ -11,9 +11,19 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#Static files (CSS, JS, Images)
+#https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static'
+
+#Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -148,3 +158,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '//'
 
 SITE_ID = 2
+
+#heroku
+django_heroku.settings(locals())
